b234d9499efe4f01f182ec64ab0168a2085e10a3
aktech27 <ahamedkbr85@gmail.com>
2021-10-03 12:01:50 +0530

    Added custom input for register

---
+++ src/components/Register/RegisterInput.jsx
@@ -0,0 +1,52 @@
+import { TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import useValidator from "../hooks/useValidator";
+
+const RegisterInput = ({
+  id,
+  label,
+  placeholder,
+  type,
+  validationType,
+  setState,
+  defaultValue,
+}) => {
+  const [helperText, setHelperText] = useState("");
+  let validator = useValidator(validationType);
+  const handleChange = (value) => {
+    //checking if validation type is confirm password and passing parameters respectively
+    let [isValid, validationText] =
+      validationType !== "confirmpassword"
+        ? validator(value)
+        : validator(value, document.querySelector("#password").value);
+    if (!isValid) {
+      setHelperText(validationText);
+      setState((prev) => ({ ...prev, [id]: "" }));
+    } else {
+      setState((prev) => ({ ...prev, [id]: value }));
+      setHelperText("");
+    }
+  };
+
+  return (
+    <TextField
+      id={id}
+      label={label}
+      placeholder={placeholder}
+      type={type}
+      defaultValue={defaultValue}
+      variant="outlined"
+      style={{ margin: "10px", width: "90%" }}
+      error={helperText ? true : false}
+      helperText={helperText}
+      onFocus={(e) => {
+        handleChange(e.target.value);
+      }}
+      onChange={(e) => {
+        handleChange(e.target.value);
+      }}
+    />
+  );
+};
+
+export default RegisterInput;
